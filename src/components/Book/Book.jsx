b29414019f5f87b1f3dca5694ffd1d3da7cfe523
RassielMex <rhcp94.rm@gmail.com>
2022-08-19 23:30:25 -0500

    spinner added

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import "./Book.css";
-import serie1 from "../../img/serie1.jpeg";
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { useState } from "react";
+import { TailSpin } from "react-loader-spinner";
 
 const Book = ({ book }) => {
-  const { id } = book;
+  const [showImg, setShowImg] = useState(false);
+  const { id, img } = book;
+  useEffect(() => {
+    const imageSource = new Image();
+    imageSource.src = img;
+    imageSource.onload = () => {
+      setShowImg(true);
+    };
+  }, [img]);
+
   return (
     <div className="book">
       <Link to={`/detail/${id}`}>
-        <img src={book.img || serie1} alt="serie" />
+        {showImg && <img src={img} alt="serie" />}
+        {!showImg && <TailSpin color="#FFFF" height={40} width={40} />}
       </Link>
     </div>
   );
